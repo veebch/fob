8b2a905895e1139cdab1f296e2a71c188f4671cf
veebch <mart@veeb.ch>
2022-01-06 12:47:53 +0100

    initial add

--- fobbed.py
+++ fobbed.py
@@ -6,14 +6,16 @@ import qrcode
 import time
 from PIL import Image,ImageDraw,ImageFont
 picdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-if os.path.exists(libdir):
-    sys.path.append(libdir)
+configfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.yaml') 
 
 try:
-    qrstring="REPLACE THIS WITH THE LONG STRING THAT STARTS WITH HC1"
+    with open(configfile) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    
+    qrstring=config['string'] # gets the string from config.yaml, if it does no already exist, 'cp config_example.yaml config.yaml'
     img = qrcode.make(qrstring,border=2)    
     img.thumbnail((200,200)) # resize to match the dimensions of the epaper    
-    img.save(os.path.join(picdir,"covidqr.bmp")) # save the image so that it can be transferred using an app
+    img.save(os.path.join(picdir,"certificateqr.bmp")) # save the image so that it can be transferred using an app
 
 except IOError as e:
     logging.info(e)
