c8d8dccc4d03ace58a68119600eee251ec35c19b
veebch <mart@veeb.ch>
2022-04-27 11:34:22 +0200

    tweaks to encoding

--- fob.py
+++ fob.py
@@ -4,14 +4,16 @@ import sys
 import os
 import qrcode
 import yaml
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 from PIL import Image,ImageDraw,ImageFont
 picdir = os.path.dirname(os.path.realpath(__file__))
 configfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.yaml')
 qr = qrcode.QRCode(
     version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    error_correction=qrcode.constants.ERROR_CORRECT_M, # medium error connection
     box_size=10,
-    border=4,
+    border=2,
 )
 try:
     with open(configfile) as f:
@@ -20,7 +22,7 @@ try:
     qrstring=config['string'] # gets the string from config.yaml, if it does no already exist, 'cp config_example.yaml config.yaml'
     print("Encoding:",qrstring)
     qr.add_data(qrstring)
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color="black", back_color="white",image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer())
     img.thumbnail((200,200)) # resize to match the dimensions of the epaper    
     img.save(os.path.join(picdir,"certificateqr.bmp")) # save the image so that it can be transferred using an app
     print('Encoded the string from config.yaml into the QR code in the file certificateqr.bmp')
