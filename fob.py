2325592a013484b1d485dc6cd4a1e2cfa73f4a81
veebch <mart@veeb.ch>
2022-04-27 11:26:25 +0200

    tweaks to encoding

--- fob.py
+++ fob.py
@@ -6,15 +6,22 @@ import qrcode
 import yaml
 from PIL import Image,ImageDraw,ImageFont
 picdir = os.path.dirname(os.path.realpath(__file__))
-configfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.yaml') 
-
+configfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.yaml')
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
 try:
     with open(configfile) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     
     qrstring=config['string'] # gets the string from config.yaml, if it does no already exist, 'cp config_example.yaml config.yaml'
-    img = qrcode.make(qrstring)    
-    img=img.thumbnail((200,200)) # resize to match the dimensions of the epaper    
+    print("Encoding:",qrstring)
+    qr.add_data(qrstring)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.thumbnail((200,200)) # resize to match the dimensions of the epaper    
     img.save(os.path.join(picdir,"certificateqr.bmp")) # save the image so that it can be transferred using an app
     print('Encoded the string from config.yaml into the QR code in the file certificateqr.bmp')
 except IOError as e:
